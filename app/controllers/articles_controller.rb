5b096fe84b0a7313f444a63319f6371a3873bd83
Matt <matthewmorgante@gmail.com>
2015-06-01 21:33:04 -0500

    updated show actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,16 +6,20 @@ class ArticlesController < ApplicationController
     redirect_to @article
   end 
 
-  private 
-    def article_params 
-      params.require(:article).permit(:title, :text)
-    end 
-
   def show 
     @article = Article.find(params[:id])
   end   
 
   def new 
   end 
+
+  def index
+    @articles = Article.all
+  end
+
+  private 
+    def article_params 
+      params.require(:article).permit(:title, :text)
+    end 
 
 end
