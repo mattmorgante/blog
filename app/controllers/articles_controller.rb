ebc241fa2d558dedfe242ef8275d53eff56b4671
Matt <matthewmorgante@gmail.com>
2015-06-05 10:26:43 -0500

    comment functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,21 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+
+  def show 
+    @article = Article.find(params[:id])
+  end   
+
+  def new 
+    @article = Article.new 
+  end 
+
+  def edit
+    @article = Article.find(params[:id])
+  end   
+
   def create 
     @article = Article.new(article_params)
   
@@ -10,17 +27,22 @@ class ArticlesController < ApplicationController
     end 
   end 
 
-  def show 
+  def update
     @article = Article.find(params[:id])
-  end   
 
-  def new 
-    @article = Article.new 
-  end 
-
-  def index
-    @articles = Article.all
+    if @article.update(article_params)
+      redirect_to @article 
+    else 
+      render 'edit'
+    end 
   end
+
+  def destroy 
+    @article = Article.find(params[:id])
+    @article.destroy 
+
+    redirect_to articles_path
+  end 
 
   private 
     def article_params 
